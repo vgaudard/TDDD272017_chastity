f83556abc9f20603bcf6d97e8cd916ff2ab2b09c
Victor Gaudard <vicga263@student.liu.se>
2017-05-05 17:29:01 +0200

    React boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+    render () {
+        return (
+            <p>Nothing to see here</p>
+        );
+    }
+}
+
+export default App;
